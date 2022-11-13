a82e64a3cf828ee54e5c3673bba0a9e7057adbaa
Elliot Potter <elliotpotter496@gmail.com>
2022-11-13 14:12:52 -0500

    BFS seems to be working for the explorer

--- simple_maze_exploring/setup.py
+++ simple_maze_exploring/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['simple_flocking'],
+    packages=['simple_maze_exploring'],
     package_dir={'': 'src'}
 )
 
