ba6a92cbe7416e955053ea8b907bf61a9def697e
Elliot Potter <elliotpotter496@gmail.com>
2022-11-13 18:14:33 -0500

    Fixed another quick bug

--- simple_maze_exploring/src/simple_maze_exploring/drive_line.py
+++ simple_maze_exploring/src/simple_maze_exploring/drive_line.py
@@ -14,8 +14,8 @@ import numpy
 
 # Constants.
 FREQUENCY = 10 #Hz.
-VELOCITY = 1 #m/s
-ANGULAR_VELOCITY = 1 #radians / sec
+VELOCITY = 0.5 #m/s
+ANGULAR_VELOCITY = 0.5 #radians / sec
 DURATION = 5 #s how long the message should be published.
 
 
